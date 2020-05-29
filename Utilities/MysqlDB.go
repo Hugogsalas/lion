53d1dedbd751749ce758685265aecd7cd3411b03
Emillie <eflores-11@hotmail.com>
2020-05-29 16:58:59 -0500

    Emi cambios bajados

--- Utilities/MysqlDB.go
+++ Utilities/MysqlDB.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 )
 
 //ExecuteCommand : Metodo de execucion de un query que no retorna nada
