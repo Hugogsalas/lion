8e179014f36ba06df0c435bc5ee256ff9bdd96c8
dooomsdayAlan <alanalexisaguilar@gmail.com>
2020-05-31 14:38:17 -0500

    merengueAlan Sala

--- Controllers/StanController.go
+++ Controllers/StanController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 
@@ -46,4 +47,84 @@ func CreateStan(writter http.ResponseWriter, request *http.Request) {
 	writter.Header().Set("Content-Type", "application/json")
 	writter.Write(payload)
 	return
+}
+
+
+//GetStan : Metodo que regresa Stanes segun parametros
+func GetStan(writter http.ResponseWriter, request *http.Request) {
+	var Stan models.Stan
+	err := json.NewDecoder(request.Body).Decode(&Stan)
+	jsonResponse := simplejson.New()
+	if err == nil {
+
+		var StanValues []interface{}
+		var StanStrings []string
+		StanValues = utilities.ObjectValues(Stan)
+		StanStrings = utilities.ObjectFields(Stan)
+
+
+		//Limpia de los atributos del objeto
+	
+		for i := 0; i < 3; i++ {
+			if StanValues[i] == 0 {
+				StanValues[i] = nil
+			}
+		}
+
+		StanRows, err := utilities.GetObject([]string{"Stan"}, nil, StanStrings, StanValues)
+		if err == nil {
+			StanesResultado, err := QueryToStan(StanRows)
+			if err == nil {
+				if len(StanesResultado) > 0 {
+					jsonResponse.Set("Exito", true)
+					jsonResponse.Set("Message", "Stanes encontrados")
+					jsonResponse.Set("Stanes", StanesResultado)
+				} else {
+					jsonResponse.Set("Exito", false)
+					jsonResponse.Set("Message", "No se encontraron Stanes")
+				}
+			} else {
+				jsonResponse.Set("Exito", false)
+				jsonResponse.Set("Message", err.Error())
+			}
+
+		} else {
+			jsonResponse.Set("Exito", false)
+			jsonResponse.Set("Message", err.Error())
+		}
+
+	} else {
+		jsonResponse.Set("Exito", false)
+		jsonResponse.Set("Message", err.Error())
+	}
+
+	payload, err := jsonResponse.MarshalJSON()
+	writter.Header().Set("Content-Type", "application/json")
+	writter.Write(payload)
+	return
+}
+
+//QueryToStan : Metodo que transforma la consulta a objetos Stan
+func QueryToStan(result *sql.Rows) ([]models.Stan, error) {
+	var StanAux models.Stan
+	var recipents []models.Stan
+	for result.Next() {
+		err := result.Scan(&StanAux.ID, &StanAux.IDEditorial, &StanAux.Numero )
+		if err != nil {
+			return nil, err
+		}
+		recipents = append(recipents, StanAux)
+	}
+	return recipents, nil
+}
+
+//StanesToInterfaces : metodo que transforma un arreglo de Stanes en interfaces
+func StanesToInterfaces(Stanes []models.Stan) []interface{} {
+	var arrayInterface []interface{}
+	for i:=0;i<len(Stanes);i++{
+		var StanInterface interface{}
+		StanInterface=Stanes[i]
+		arrayInterface=append(arrayInterface,StanInterface)
+	}
+	return arrayInterface
 }
